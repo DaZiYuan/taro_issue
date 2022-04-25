afef61245dd44f6b81b89d6e807a6200200f6387
DaZiYuan <mscoder@hotmail.com>
2022-04-25 11:57:50 +0800

    添加注释

--- myApp/src/pages/index/index.config.ts
+++ myApp/src/pages/index/index.config.ts
@@ -1,5 +1,5 @@
-import { config } from "@/config/data";
-// import { config } from "../../config/data";
+import { config } from "@/config/data"; //报错
+// import { config } from "../../config/data";  正常
 export default definePageConfig({
   navigationBarTitleText: config.title,
 });
